8345a9cc1d0ce8164982a7c4caee107fbd3b2c26
Robert Vollmert <rob@vllmrt.net>
2019-06-28 16:51:01 +0200

    pzprnode service

--- pzprnode.scm
+++ pzprnode.scm
@@ -1,12 +1,22 @@
 (define-module (pzprnode)
+  #:use-module (ice-9 match)
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (gnu system accounts)
+  #:use-module (gnu system shadow)
+  #:use-module (gnu services)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu packages admin)
   #:use-module (gnu packages node)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages imagemagick)
   #:use-module (gnu packages gnome) ;librsvg
-  #:use-module (guix build-system trivial))
+  #:use-module (guix build-system trivial)
+
+  #:use-module (pzpr))
 
 (define-public pzprnode
   (package
@@ -131,3 +141,57 @@
     (synopsis #f)
     (license #f)
     (description #f)))
+
+(define-record-type* <pzprnode-configuration>
+  pzprnode-configuration make-pzprnode-configuration
+  pzprnode-configuration?
+  (pzprnod pzprnode-configuration-pzprnode (default pzprnode)))
+
+(define %pzprnode-accounts
+  (list (user-group (name "pzprnode") (system? #t))
+        (user-account
+         (name "pzprnode")
+         (group "pzprnode")
+         (system? #t)
+         (comment "pzprnode server user")
+         (home-directory "/var/empty")
+         (shell (file-append shadow "/sbin/nologin")))))
+
+; copied over from postgrest
+(define* (logger-wrapper name exec . args)
+  "Return a derivation that builds a script to start a process with
+standard output and error redirected to syslog via logger."
+  (define exp
+    #~(begin
+        (use-modules (ice-9 popen))
+        (let* ((pid    (number->string (getpid)))
+               (logger #$(file-append inetutils "/bin/logger"))
+               (args   (list "-t" #$name (string-append "--id=" pid)))
+               (pipe   (apply open-pipe* OPEN_WRITE logger args)))
+          (dup pipe 1)
+          (dup pipe 2)
+          (execl #$exec #$exec #$@args))))
+  (program-file (string-append name "-logger") exp))
+
+(define pzprnode-shepherd-service
+  (match-lambda
+   (($ <pzprnode-configuration> pzpr-node)
+    (list (shepherd-service
+          (provision '(pzprnode))
+          (documentation "Run the pzprnode daemon.")
+          (requirement '(user-processes))
+          (start #~(make-forkexec-constructor
+                    '(#$(logger-wrapper "pzprnode" (file-append pzpr-node "/bin/pzprnode")))
+                    #:user "pzprnode"
+                    #:group "pzprnode"))
+          (stop #~(make-kill-destructor)))))))
+
+(define-public pzprnode-service-type
+  (service-type
+   (name 'pzprnode)
+   (extensions
+    (list (service-extension shepherd-root-service-type
+                             pzprnode-shepherd-service)
+          (service-extension account-service-type
+                             (const %pzprnode-accounts))))
+   (default-value (pzprnode-configuration))))
