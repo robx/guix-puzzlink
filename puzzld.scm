18013d3a1314d518a273cecd299fbb698f188eed
Robert Vollmert <rob@vllmrt.net>
2020-03-30 14:44:03 +0200

    add puzzld service

--- puzzld.scm
+++ puzzld.scm
@@ -4,6 +4,16 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages)
+  #:use-module (guix records)
+
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (gnu system accounts)
+  #:use-module (gnu system shadow)
+  #:use-module (gnu services)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu packages admin)
+  #:use-module (ice-9 match)
 
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system haskell))
@@ -28,3 +38,56 @@
     (description
      "Websocket puzzle server")
     (license license:expat)))
+
+(define-record-type* <puzzld-configuration>
+  puzzld-configuration make-puzzld-configuration
+  puzzld-configuration?
+  (puzzld puzzld-configuration-puzzld (default puzzld)))
+
+(define %puzzld-accounts
+  (list (user-group (name "puzzld") (system? #t))
+        (user-account
+         (name "puzzld")
+         (group "puzzld")
+         (system? #t)
+         (comment "puzzld server user")
+         (home-directory "/var/empty")
+         (shell (file-append shadow "/sbin/nologin")))))
+
+(define* (logger-wrapper name exec . args)
+  "Return a derivation that builds a script to start a process with
+standard output and error redirected to syslog via logger."
+  (define exp
+    #~(begin
+        (use-modules (ice-9 popen))
+        (let* ((pid    (number->string (getpid)))
+               (logger #$(file-append inetutils "/bin/logger"))
+               (args   (list "-t" #$name (string-append "--id=" pid)))
+               (pipe   (apply open-pipe* OPEN_WRITE logger args)))
+          (dup pipe 1)
+          (dup pipe 2)
+          (execl #$exec #$exec #$@args))))
+  (program-file (string-append name "-logger") exp))
+
+(define puzzld-shepherd-service
+  (match-lambda
+    (($ <puzzld-configuration> puzzld)
+     (list (shepherd-service
+            (provision '(puzzld))
+            (documentation "Run the puzzld pzprjs websocket daemon.")
+            (requirement '(user-processes))
+            (start #~(make-forkexec-constructor
+                      '(#$(logger-wrapper "puzzld" (file-append puzzld "/bin/puzzld")))
+                      #:user "puzzld"
+                      #:group "puzzld"))
+            (stop #~(make-kill-destructor)))))))
+
+(define-public puzzld-service-type
+  (service-type
+   (name 'puzzld)
+   (extensions
+    (list (service-extension shepherd-root-service-type
+                             puzzld-shepherd-service)
+          (service-extension account-service-type
+                             (const %puzzld-accounts))))
+   (default-value (puzzld-configuration))))
